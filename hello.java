508e56b1c4a7617a00ddef383da9a7cea37ffdb3
SukeerLedezma <sukeerledezma@gmail.com>
2024-03-21 10:33:42 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,17 +1,15 @@
 // a simple program to practice common Github commands 
-
 public class hello {
 
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = " Sukeer";
 
        // delete this line
-       System.out.println("Modify this code");
+       System.out.println("Hello, I am" + firstName);
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
     }
 }
 
